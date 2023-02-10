8846545aab31be82b4eaa4fc73fa4ccbc236c80f
hx2ryu <hx2ryu@gmail.com>
2023-02-10 22:02:22 +0900

    chore: setup firebase

--- ios/HeybroApp/AppDelegate.mm
+++ ios/HeybroApp/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -57,6 +58,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
