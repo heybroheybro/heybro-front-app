ebac53d11ec4de88585aeabd148415f1ef63d92c
hx2ryu <hx2ryu@gmail.com>
2023-02-12 18:06:28 +0900

    feat: implement input hook

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+type ReturnType = [string, (value: string) => void]
+
+export function useInput<T>(initialValue = '', maxLength = 200): ReturnType {
+    const [value, setValue] = useState<string>(initialValue)
+
+    const handleChangeText = (text: string) => {
+        const nextValue = text.substring(0, maxLength)
+        setValue(nextValue)
+    }
+
+    return [value, handleChangeText]
+}
