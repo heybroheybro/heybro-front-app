18311c2e61a676b8eb651a2f7ed81df50c8ce018
hx2ryu <hx2ryu@gmail.com>
2023-02-26 21:58:01 +0900

    fix: modify api interceptor logic

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,28 +1,30 @@
+import AuthStorage from '@heybro/storages/auth/AuthStorage'
 import axios from 'axios'
-import axiosInherit from 'axios-inherit'
 import * as AxiosLogger from 'axios-logger'
 import Config from 'react-native-config'
 
-axiosInherit(axios)
+const client = axios.create({
+    baseURL: Config.SERVICE_BASE_URL,
+})
+client.interceptors.request.use(
+    async (config) => {
+        if (config.url?.includes('/auth') || config.url?.includes('/city')) return AxiosLogger.requestLogger(config)
 
-axios.defaults.baseURL = Config.SERVICE_BASE_URL
-axios.interceptors.request.use(AxiosLogger.requestLogger)
-axios.interceptors.response.use(AxiosLogger.responseLogger)
+        const { accessToken } = await AuthStorage.get()
+        return AxiosLogger.requestLogger({
+            ...config,
+            headers: {
+                ...config.headers,
+                ['access_token']: accessToken,
+            },
+        })
+    },
+    (error) => Promise.reject(error),
+)
 
-export function initialAxiosAuthorizationInterceptor(accessToken: string) {
-    axios.interceptors.request.use(
-        (config) => {
-            if (config.url?.includes('/auth') || config.url?.includes('/city')) return config
-            return {
-                ...config,
-                headers: {
-                    ...config.headers,
-                    Authorization: accessToken,
-                },
-            }
-        },
-        (error) => Promise.reject(error),
-    )
-}
+client.interceptors.response.use(
+    (res) => AxiosLogger.responseLogger(res),
+    (error) => Promise.reject(error),
+)
 
-export default axios.create()
+export default client
