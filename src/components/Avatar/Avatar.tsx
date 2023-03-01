e4d8a3d600148020a7c934cd4d044f61fecbf72e
hx2ryu <hx2ryu@gmail.com>
2023-03-01 20:15:30 +0900

    refactor: enhance more customizable style

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { Image, TouchableOpacityProps } from 'react-native'
-import { AvatarWrapper } from './styles'
-import DefaultProfileImg from '@heybro/assets/images/img-profile-default.png'
+import { TouchableOpacityProps } from 'react-native'
+import { AvatarImage, AvatarWrapper } from './styles'
+import DefaultProfileLargeImg from '@heybro/assets/images/img-profile-default.png'
+import DefaultProfileSmallImg from '@heybro/assets/images/ic-my-inactive.png'
 
 type Props = {
     imageUrl?: string
+    size?: 'small' | 'large'
 } & TouchableOpacityProps
 
-export function Avatar({ imageUrl, ...props }: Props) {
+export function Avatar({ imageUrl, size = 'large', ...props }: Props) {
     return (
         <AvatarWrapper activeOpacity={0.8} {...props}>
-            <Image source={imageUrl ? { uri: imageUrl } : DefaultProfileImg} />
+            <AvatarImage
+                source={
+                    imageUrl ? { uri: imageUrl } : size === 'large' ? DefaultProfileLargeImg : DefaultProfileSmallImg
+                }
+            />
         </AvatarWrapper>
     )
 }
--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -4,3 +4,9 @@ export const AvatarWrapper = styled.TouchableOpacity`
     border-radius: 999px;
     align-self: flex-start;
 `
+
+export const AvatarImage = styled.Image`
+    width: 63px;
+    height: 63px;
+    border-radius: 999px;
+`
