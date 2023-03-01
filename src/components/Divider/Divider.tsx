65a220913337d20018331d8073e00ca23974d641
hx2ryu <hx2ryu@gmail.com>
2023-03-01 20:15:17 +0900

    refactor: enhance more customizable style

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import { useGlobalTheme } from '@heybro/hooks/useGlobalTheme'
 import { View, ViewProps } from 'react-native'
+import { DefaultTheme } from 'styled-components/native'
 
 type Props = {
-    color?: string
+    color?: keyof DefaultTheme['colors']
     direction: 'row' | 'column'
+    size?: number
 } & ViewProps
 
-export function Divider({ color, direction, style }: Props) {
+export function Divider({ color = 'white', direction, size = 8, style }: Props) {
+    const { colors } = useGlobalTheme()
     const { width, height } =
         direction === 'column'
             ? {
-                  width: 8,
+                  width: size,
                   height: '100%',
               }
             : {
                   width: '100%',
-                  height: 8,
+                  height: size,
               }
-    return <View style={[{ backgroundColor: color, width, height }, style]} />
+    return <View style={[{ backgroundColor: colors[color], width, height }, style]} />
 }
