145c7a90b733b6526d3d017c522f30580f1d61a5
hx2ryu <hx2ryu@gmail.com>
2023-03-04 19:42:37 +0900

    feat: implement FloatingMenu

---
+++ src/screens/HomeScreen/components/FloatingMenu/FloatingMenu.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { FloatingAction, IActionProps } from 'react-native-floating-action'
+import { useIntl } from 'react-intl'
+import { useGlobalTheme } from '@heybro/hooks/useGlobalTheme'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { RootParamList } from '@heybro/navigation/types'
+import QuestionIcon from '@heybro/assets/images/ic-hello.png'
+import InformingIcon from '@heybro/assets/images/ic-message.png'
+
+export function FloatingMenu() {
+    const intl = useIntl()
+    const { colors } = useGlobalTheme()
+    const { navigate } = useNavigation<NativeStackNavigationProp<RootParamList>>()
+
+    const [isOpened, setIsOpened] = useState(false)
+    const actions: IActionProps[] = [
+        {
+            text: intl.formatMessage({ id: '@HOME-007' }),
+            name: 'QUESTION',
+            icon: QuestionIcon,
+            textBackground: 'transparent',
+            textColor: colors.white,
+            color: colors.primary,
+            tintColor: '',
+        },
+        {
+            text: intl.formatMessage({ id: '@HOME-008' }),
+            name: 'TIP',
+            icon: InformingIcon,
+            textBackground: 'transparent',
+            textColor: colors.white,
+            color: colors.primary,
+            tintColor: '',
+        },
+    ]
+
+    const handlePressMenuItem = (name?: string) => {
+        if (name === actions[0].name) {
+            navigate('HomeNav', { screen: 'WritePost', params: { type: 'QUESTION' } })
+        } else if (name === actions[1].name) {
+            navigate('HomeNav', { screen: 'WritePost', params: { type: 'TIP' } })
+        }
+    }
+
+    return (
+        <FloatingAction
+            actions={actions}
+            color={isOpened ? colors.grayscale400 : colors.primary}
+            onStateChange={() => setIsOpened((prev) => !prev)}
+            onPressItem={handlePressMenuItem}
+        />
+    )
+}
---
+++ src/screens/HomeScreen/components/FloatingMenu/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native'
+
+export const MenuContainer = styled.View`
+    position: absolute;
+    bottom: 0;
+    right: 50px;
+`
